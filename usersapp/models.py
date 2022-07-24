383a5d9038bb45f7e79a5a20bb86619a61378041
Mahadev <mahadev.clever@gmail.com>
2022-07-24 21:34:37 +0530

    image fix

--- usersapp/models.py
+++ usersapp/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
     profile_photo = models.ImageField(
-        upload_to="users/", blank=True, null=True,default="defaulu_user.png")
+        upload_to="users/", blank=True, null=True,default="users/default_user.png")
     email = models.EmailField(unique=True, max_length=200)
