678e41d87794ac8901330f6bb5e4bd4f7d0f1179
Mahadev <mahadev.clever@gmail.com>
2022-07-24 20:26:20 +0530

    default_image added

--- usersapp/models.py
+++ usersapp/models.py
@@ -1,8 +1,9 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class UserProfile(AbstractUser):
     profile_photo = models.ImageField(
-        upload_to="users/", blank=True, null=True)
+        upload_to="users/", blank=True, null=True,default="defaulu_user.png")
     email = models.EmailField(unique=True, max_length=200)
